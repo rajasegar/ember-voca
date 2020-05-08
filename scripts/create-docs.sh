d4479dbb6bb9539a07ad6d56f3dde9ad682ed810
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-08 09:14:21 +0530

    chore: add routes for demo app

--- scripts/create-docs.sh
+++ scripts/create-docs.sh
@@ -1,32 +1,56 @@
 #!/bin/sh
 
+create_routes() {
+helper=$1
+capitalized_name=$2
+
+# create route
+route_name="tests/dummy/app/routes/$helper.js"
+touch "$route_name"
+
+# write route content
+echo "import Route from '@ember/routing/route';
+
+export default class ${capitalized_name}Route extends Route {
+}" >> $route_name
+
+# create route template
+touch "tests/dummy/app/templates/$helper.hbs"
+
+echo  "this.route('$helper');" >> tests/dummy/app/router.js
+
+}
+
 create_docs() {
-  helper_name="chance-$1"
-  # Write examples to application.hbs
-  echo  "
+  helper_name="$1"
+
+  echo "
   <h3>$helper_name:</h3>
   <p>Description for $helper_name goes here.</p>
   <p>
   <code>
-  \{{ $helper_name }}
+  \{{ $helper_name \"hello world\"}}
   </code>
   </p>
   <p>
   <code>
-  => {{ $helper_name }}
+  // => {{ $helper_name \"hello world\"}}
   </code>
-  </p>
-  <p>With named arguments (options) :</p>
-  <p>
-  <code>
-  \{{ $helper_name }}
-  </code>
-  </p>
-  " >> tests/dummy/app/templates/application.hbs
+  </p>" >> "tests/dummy/app/templates/$helper_name.hbs"
+
+
 }
 
 while read -r helper; do
-  echo $helper
+
+  snake_case=$(echo $helper | gsed -r 's/([a-z0-9])([A-Z])/\1-\L\2/g')
+  helper_name="voca-$snake_case"
+  caps="Voca-$snake_case"
+  route_name=$(echo $caps | awk -F - '{printf "%s", $1; for(i=2; i<=NF; i++) printf "%s", toupper(substr($i,1,1)) substr($i,2); print"";}')
+  echo $helper_name
+  echo $route_name
   echo "=================="
-  create_docs $helper
+  #create_routes $helper_name $route_name
+  #create_docs $helper_name
+  echo "<li><LinkTo @route=\"$helper_name\">$helper_name</LinkTo></li>" >> tests/dummy/app/templates/application.hbs
 done < $1
---
+++ tests/dummy/app/routes/voca-slice.js
@@ -0,0 +1,4 @@
+import Route from '@ember/routing/route';
+
+export default class VocaSliceRoute extends Route {
+}
