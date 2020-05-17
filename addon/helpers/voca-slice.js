1540b1acec69cef73dd90b8cda16bd5634fde2c6
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-17 09:54:01 +0530

    fix: update params for slice helper

--- addon/helpers/voca-slice.js
+++ addon/helpers/voca-slice.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 import { slice } from 'voca';
 
-export default helper(function vocaSlice(params/*, hash */ ) {
-  return slice(params[0]);
+export default helper(function vocaSlice(params, { start, end } ) {
+  return slice(params[0], start, end);
 });
