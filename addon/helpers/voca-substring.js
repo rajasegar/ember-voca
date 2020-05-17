5f177a8e18ae1189e72344ff882c32bf88bc154a
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-17 08:35:59 +0530

    fix: update substring helpers to have arguments

--- addon/helpers/voca-substring.js
+++ addon/helpers/voca-substring.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 import { substring } from 'voca';
 
-export default helper(function vocaSubstring(params/*, hash */ ) {
-  return substring(params[0]);
+export default helper(function vocaSubstring(params ) {
+  return substring(params[0], params[1], params[2]);
 });
