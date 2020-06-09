8182f5f76b12760569782e83d0291035758f095c
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-06-09 16:43:30 +0530

    fix: voca-ends with to add end parameter

--- addon/helpers/voca-ends-with.js
+++ addon/helpers/voca-ends-with.js
@@ -2,5 +2,5 @@ import { helper } from '@ember/component/helper';
 import { endsWith } from 'voca';
 
 export default helper(function vocaEndsWith(params/*, hash */ ) {
-  return endsWith(params[0]);
+  return endsWith(params[0], params[1]);
 });
